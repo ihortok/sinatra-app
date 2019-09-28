53109c781f3df336e5c45332396a419a95222df5
ihortok <itokaryk@gmail.com>
2019-09-28 23:58:50 +0300

    books crud (new,  index)

--- app.rb
+++ app.rb
@@ -1,7 +1,22 @@
 require 'rubygems'
 require 'sinatra'
+require 'sinatra/activerecord'
 require 'haml'
 
+current_dir = Dir.pwd
+Dir["#{current_dir}/models/*.rb"].each { |file| require file }
+
 get '/' do
+  @books = Book.all
   haml :index
+end
+
+get '/books/new' do
+  haml :new
+end
+
+post '/books' do
+  book = Book.new(params[:book])
+  book.save
+  redirect '/books'
 end
