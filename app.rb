5d1fa335e570bd0eb2dade452791e1034bb42e76
ihortok <itokaryk@gmail.com>
2019-10-01 20:56:57 +0300

    books all actions added

--- app.rb
+++ app.rb
@@ -12,12 +12,34 @@ get '/books' do
   haml :'books/index'
 end
 
+post '/books' do
+  book = Book.new(params[:book])
+  book.save
+  redirect '/books'
+end
+
 get '/books/new' do
   haml :'books/new'
 end
 
-post '/books' do
-  book = Book.new(params[:book])
-  book.save
+get '/books/:id/edit' do
+  @book = Book.find(params[:id])
+  haml :'books/edit'
+end
+
+patch '/books/:id' do
+  @book = Book.find(params[:id])
+  @book.update(params[:book])
+  redirect "/books/#{@book.id}"
+end
+
+get '/books/:id' do
+  @book = Book.find(params[:id])
+  haml :'books/show'
+end
+
+delete '/books/:id' do
+  @book = Book.find(params[:id])
+  @book.destroy
   redirect '/books'
 end
